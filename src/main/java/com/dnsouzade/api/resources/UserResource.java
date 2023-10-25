aa8522053be85166734db5450cbde66a616fd17a
Daniel Souza <workdndsza@gmail.com>
2023-10-24 22:37:12 -0300

    criando a entidade User fix

--- src/main/java/com/dnsouzade/api/resources/UserResource.java
+++ src/main/java/com/dnsouzade/api/resources/UserResource.java
@@ -3,6 +3,8 @@ package com.dnsouzade.api.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +16,12 @@ import com.dnsouzade.api.domain.User;
 public class UserResource {
 
 	@GetMapping
-	public List<User> findAll() {
+	public ResponseEntity<List<User>> findAll() {
 		User maria = new User("1", "Maria Silva", "maria@maria.com");
 		User alex = new User("2", "Alex Silva", "alex@maria.com");
 		List<User> list = new ArrayList<>();
 		list.addAll(Arrays.asList(maria, alex));
-		return list;
+		return ResponseEntity.ok().body(list);
 		
 	}
 }
