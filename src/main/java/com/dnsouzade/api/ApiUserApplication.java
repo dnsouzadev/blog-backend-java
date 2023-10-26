147d4742ef48c237687d5a87b20cd472b71a28de
Daniel Souza <workdndsza@gmail.com>
2023-10-25 22:20:11 -0300

    fixing the bug

--- src/main/java/com/dnsouzade/api/ApiUserApplication.java
+++ src/main/java/com/dnsouzade/api/ApiUserApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.dnsouzade.api.repository")
 public class ApiUserApplication {
 
 	public static void main(String[] args) {
