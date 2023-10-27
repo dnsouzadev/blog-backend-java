996ee130a38a18387df799d456d50522055718bc
Daniel Souza <workdndsza@gmail.com>
2023-10-27 00:08:08 -0300

    criando o metodo Delete

--- src/main/java/com/dnsouzade/api/services/UserService.java
+++ src/main/java/com/dnsouzade/api/services/UserService.java
@@ -32,4 +32,13 @@ public class UserService {
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
+
+    public void delete(String id) {
+        findById(id);
+        repo.deleteById(id);
+    }
+
+    public User update(User obj) {
+
+    }
 }
