fe7d4cf7e547a4464e8490a0f830e8e051b2c4e8
Daniel Souza <workdndsza@gmail.com>
2023-10-28 07:50:58 -0300

    projecao de dados do autor com DTO

--- src/main/java/com/dnsouzade/api/config/Instantiation.java
+++ src/main/java/com/dnsouzade/api/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.dnsouzade.api.config;
 
 import com.dnsouzade.api.domain.Post;
 import com.dnsouzade.api.domain.User;
+import com.dnsouzade.api.dto.AuthorDTO;
 import com.dnsouzade.api.repository.PostRepository;
 import com.dnsouzade.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,8 +43,8 @@ public class Instantiation implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!",
-                maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+                new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.deleteAll();
 
