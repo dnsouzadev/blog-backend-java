863c796f99459c9bd887faefc56c9975d8bcbb55
Daniel Souza <workdndsza@gmail.com>
2023-10-28 11:42:16 -0300

    referenciando os posts do usuario

--- src/main/java/com/dnsouzade/api/config/Instantiation.java
+++ src/main/java/com/dnsouzade/api/config/Instantiation.java
@@ -47,8 +47,10 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.deleteAll();
-
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
 
 
 
