26fad6b352726f45fb6650a867cabb816c197db7
Daniel Souza <workdndsza@gmail.com>
2023-10-29 21:42:42 -0300

    consulta simples com query methods

--- src/main/java/com/dnsouzade/api/resources/PostResource.java
+++ src/main/java/com/dnsouzade/api/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.dnsouzade.api.resources;
 import com.dnsouzade.api.domain.Post;
 import com.dnsouzade.api.domain.User;
 import com.dnsouzade.api.dto.UserDTO;
+import com.dnsouzade.api.resources.util.URL;
 import com.dnsouzade.api.services.PostService;
 import com.dnsouzade.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,5 +26,12 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
